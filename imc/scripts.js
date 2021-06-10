496b308bbc4330c98411f8a41286975931ce6ff1
johan_mitma12 <avira365@hotmail.com>
2021-06-10 14:29:45 -0500

    se agrego la referencia del calculo IMC

---
+++ imc/scripts.js
@@ -0,0 +1,47 @@
+
+var imc_result=document.getElementById("imc_result");
+var estatura_component=document.getElementById("estatura_content");
+var masa_component=document.getElementById("peso_content");
+
+function calcular(){
+    //extraemos las variables
+    imc_result.innerHTML="";
+    let estatura=estatura_component.value;
+    let masa=masa_component.value;
+    console.log(estatura,masa);
+    if(estatura=="" || masa==""){
+        alert("Faltan datos para hacer el calculo IMC");
+    }else{
+            //convertimos la cadena en flotante
+        estatura=parseFloat(estatura);
+        masa=parseFloat(masa);
+        let res=masa/Math.pow(estatura,2);
+        imc_result.innerHTML="<strong>El valor del IMC es: "+ res.toFixed(3)+"</strong>";
+        /*
+        Peso inferior al normal: Menos de 18.5
+        Peso superior al normal: 25.0 – 29.9
+        Obesidad: Más de 30.0
+        */
+       if(res<18.5){
+        imc_result.innerHTML+="<p> Peso inferior al normal</p>";
+        }else{
+            if(res<25){
+                imc_result.innerHTML+="<p> Peso Normal</p>";
+            }else{
+                if(res<30){
+                    imc_result.innerHTML+="<p> Peso Superior al Normal</p>";
+                }else{
+                    imc_result.innerHTML+="<p> Obesidad </p>";   
+                }
+            }
+        }
+    
+
+}}
+
+function limpiar(){
+    imc_result.innerHTML="";
+    estatura_component.value="";
+    masa_component.value="";
+    $(".alert").alert();
+}
