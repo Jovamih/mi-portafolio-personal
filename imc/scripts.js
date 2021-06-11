cc1c0e55a464dab7faf08fb8bc2409d285722369
johan_mitma12 <avira365@hotmail.com>
2021-06-11 01:35:18 -0500

    se mejora la seguridad de validacion de datos del IMC

--- imc/scripts.js
+++ imc/scripts.js
@@ -9,31 +9,36 @@ function calcular(){
     let estatura=estatura_component.value;
     let masa=masa_component.value;
     console.log(estatura,masa);
-    if(estatura=="" || masa==""){
-        alert("Faltan datos para hacer el calculo IMC");
+    if(estatura===""|| masa===""){
+        alert("Ingrese todos los datos para el calculo IMC");
     }else{
             //convertimos la cadena en flotante
-        estatura=parseFloat(estatura);
+        
+        estatura=parseFloat(estatura); 
         masa=parseFloat(masa);
-        let res=masa/Math.pow(estatura,2);
-        imc_result.innerHTML="<strong>El valor del IMC es: "+ res.toFixed(3)+"</strong>";
-        /*
-        Peso inferior al normal: Menos de 18.5
-        Peso superior al normal: 25.0 – 29.9
-        Obesidad: Más de 30.0
-        */
-       if(res<18.5){
-        imc_result.innerHTML+="<p> Peso inferior al normal</p>";
-        }else{
-            if(res<25){
-                imc_result.innerHTML+="<p> Peso Normal</p>";
-            }else{
-                if(res<30){
-                    imc_result.innerHTML+="<p> Peso Superior al Normal</p>";
+        if(!isNaN(estatura) && !isNaN(masa)){
+            let res=masa/Math.pow(estatura,2);
+            imc_result.innerHTML="<strong>El valor del IMC es: "+ res.toFixed(3)+"</strong>";
+            /*
+            Peso inferior al normal: Menos de 18.5
+            Peso superior al normal: 25.0 – 29.9
+            Obesidad: Más de 30.0
+            */
+            if(res<18.5){
+                imc_result.innerHTML+="<p> Peso inferior al normal</p>";
                 }else{
-                    imc_result.innerHTML+="<p> Obesidad </p>";   
+                    if(res<25){
+                        imc_result.innerHTML+="<p> Peso Normal</p>";
+                    }else{
+                        if(res<30){
+                            imc_result.innerHTML+="<p> Peso Superior al Normal</p>";
+                        }else{
+                            imc_result.innerHTML+="<p> Obesidad </p>";   
+                        }
+                    }
                 }
-            }
+        }else{
+            alert("Datos proporcionados erroneamente");
         }
     
 
